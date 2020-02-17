fbfa3b2a742e5dcc22a4e56e36a5b53d3d03b251
David Wolff <dcwolff218@gmail.com>
2020-02-17 01:32:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,11 +3,11 @@ require 'pry'
 
 
 def reverse_each_word (string)
-  binding.pry
   new_string = string.split
   new_array = []
   new_string.collect do |i|
      new_array << i.reverse
+  binding.pry
   end
    new_array.join(" ")
 end
